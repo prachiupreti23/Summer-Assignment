c2d9c4f2d07d62bd53c920e118bd0564bfb2d303
prachiupreti23 <prachi.upreti2025@glbajajgroup.org>
2026-07-14 10:59:56 +0530

    Add files via upload

---
+++ day17/Q66Write a program to Union of arrays.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, j, found;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    int arr1[100], arr2[100];
+
+    printf("Enter elements of first array: ");
+    for(i = 0; i < n1; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of second array: ");
+    for(i = 0; i < n2; i++)
+        scanf("%d", &arr2[i]);
+
+    printf("Union of arrays:\n");
+
+    for(i = 0; i < n1; i++)
+        printf("%d ", arr1[i]);
+
+    for(i = 0; i < n2; i++) {
+        found = 0;
+        for(j = 0; j < n1; j++) {
+            if(arr2[i] == arr1[j]) {
+                found = 1;
+                break;
+            }
+        }
+        if(found == 0)
+            printf("%d ", arr2[i]);
+    }
+
+    return 0;
+}
